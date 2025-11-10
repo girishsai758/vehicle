b14638c017805daf9edffb6c91f001360a0ecfca
Girish <dmgirishsai1998@gmail.com>
2025-11-10 07:43:47 +0530

    fourth commit

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="src",
     version="0.0.1",
     author="girish sai",
-    author_email="dmgirishsai1998@gmail.com",
+    author_email="dmgirishsai19@gmail.com",
     packages=find_packages()
 )
